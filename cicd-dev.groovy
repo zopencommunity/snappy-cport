683dca559d307dcebb328e71297eaee46b09598a
TejaswiniIBM <tejaswini.r1@ibm.com>
2025-05-23 07:04:33 -0400

    Initial porting

---
+++ cicd-dev.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/snappy-cport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/snappy-cport.git'), string(name: 'PORT_DESCRIPTION', value: 'C port of the snappy compressor' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+  }
+}
